fb70cd4f8f32ac173f8de252166f453b020c8f03
Rodrigo Gonzalez <rgonzalez@kamaleon.cl>
2023-05-03 00:01:55 -0400

    Inicio presentacion web de rod

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+evenListeners();
+function evenListeners() {
+    document.addEventListener('DOMContentLoaded', saludoInicial);
+}
+
+function saludoInicial() {
+    
+    console.log("Bienvenido a la web personal de Rodrigo González Carrasco");
+    
+}
