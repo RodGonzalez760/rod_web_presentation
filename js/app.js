cdb9d3542d950979359ff5bf23281d708c557ef7
RodGonzalez <rgonzalez@kamaleon.cl>
2023-05-05 15:54:59 -0400

    fix tamaños

--- js/app.js
+++ js/app.js
@@ -9,11 +9,3 @@ function saludoInicial() {
     console.log("Bienvenido a la web personal de Rodrigo González Carrasco");
 
 }
-
-// let btnMenu = document.getElementById('btnMenu');
-// let menu = document.getElementById('navbarNav');
-// btnMenu.addEventListener('click', function () {
-//     // debugger
-//     'use strict';
-//     menu.classList.toggle('mostrar');
-// });
